a55fda6ec3366d087fbb6df4f48af2495904a78d
Ferruccio Barletta <ferruccio.barletta@gmail.com>
2023-11-04 06:25:40 -0400

    Add missing bullet update states

--- rats/src/entities/bullet.rs
+++ rats/src/entities/bullet.rs
@@ -53,26 +53,47 @@ pub fn update_bullet(bullet: &Bullet, maze: &Maze, update: u32) -> Action {
     if update < bullet.update + BULLET_UPDATE_MS {
         return Action::Nothing;
     }
-    let (row, col) = (bullet.pos.row, bullet.pos.col);
-    let (rows, cols) = (maze.rows(), maze.cols());
-    let (row, col) = match bullet.dir {
-        dir::DOWN => (row.inc(rows), col),
-        dir::DOWN_LEFT => (row.inc(rows), col.dec(cols)),
-        dir::DOWN_RIGHT => (row.inc(rows), col.inc(cols)),
-        dir::UP => (row.dec(rows), col),
-        dir::UP_LEFT => (row.dec(rows), col.dec(cols)),
-        dir::UP_RIGHT => (row.dec(rows), col.inc(cols)),
-        dir::LEFT => (row, col.dec(cols)),
-        dir::RIGHT => (row, col.inc(cols)),
-        _ => (row, col),
-    };
-    if maze.is_wall(row, col) {
-        Action::Delete
-    } else {
-        Action::Update(Entity::Bullet(Bullet {
+    let bullet = *bullet;
+    match bullet.state {
+        state::ALIVE => {
+            let (row, col) = (bullet.pos.row, bullet.pos.col);
+            let (rows, cols) = (maze.rows(), maze.cols());
+            let (row, col) = match bullet.dir {
+                dir::DOWN => (row.inc(rows), col),
+                dir::DOWN_LEFT => (row.inc(rows), col.dec(cols)),
+                dir::DOWN_RIGHT => (row.inc(rows), col.inc(cols)),
+                dir::UP => (row.dec(rows), col),
+                dir::UP_LEFT => (row.dec(rows), col.dec(cols)),
+                dir::UP_RIGHT => (row.dec(rows), col.inc(cols)),
+                dir::LEFT => (row, col.dec(cols)),
+                dir::RIGHT => (row, col.inc(cols)),
+                _ => (row, col),
+            };
+            if maze.is_wall(row, col) {
+                Action::Delete
+            } else {
+                Action::Update(Entity::Bullet(Bullet {
+                    update: update + BULLET_UPDATE_MS,
+                    pos: Position { row, col },
+                    ..bullet
+                }))
+            }
+        }
+        state::EXPLODING1 => Action::Update(Entity::Bullet(Bullet {
             update: update + BULLET_UPDATE_MS,
-            pos: Position { row, col },
-            ..*bullet
-        }))
+            state: state::EXPLODING2,
+            ..bullet
+        })),
+        state::EXPLODING2 => Action::Update(Entity::Bullet(Bullet {
+            update: update + BULLET_UPDATE_MS,
+            state: state::EXPLODING3,
+            ..bullet
+        })),
+        state::EXPLODING3 => Action::Update(Entity::Bullet(Bullet {
+            update: update + BULLET_UPDATE_MS,
+            state: state::DEAD,
+            ..bullet
+        })),
+        state::DEAD | _ => Action::Delete,
     }
 }
